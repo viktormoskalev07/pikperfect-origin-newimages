9a88bf54eebcedc15896b18569cf99e1f9c8640c
unknown <viktormoskalev07@gmail.com>
2021-08-20 15:03:12 +0300

    5.1

--- src/js/5.1photoselect.js
+++ src/js/5.1photoselect.js
@@ -1,8 +1,27 @@
 (function(){
+//tab
+        const tabsSlected =document.querySelector("#selected__js");
+        const tabAll =document.querySelector("#all__js");
+        const section = document.querySelector(".photoselect__section");
+        tabAll.addEventListener("click", function(){
+            section.classList.remove("only-selected");
+        })
+        tabsSlected.addEventListener("click", function(){
+            section.classList.add("only-selected");
+        })
 
+
+
+    //cards
+    const zoomContainer=document.querySelector(".photoselect__zoom__container");
+    const zoomFigure = zoomContainer.querySelector(".photoselect__zoom__img");
+    zoomContainer.addEventListener("click", function(){
+        zoomContainer.style="display:none";
+       zoomFigure.innerHTML="";
+    })
 const imagesParent = document.querySelector("#photoselect-parent");
 const add= (item)=>{
-    const card = item.parentNode;
+    const card = item.closest('.card-parent__js'); 
     if(card&&card.dataset.type=="card"){
 
         if(card.dataset.selected){ 
@@ -15,7 +34,7 @@ const add= (item)=>{
             card.classList.remove("deselected");
         }  
     } else {
-        console.log("card structure was broken , it not right parent node ");
+        console.log("card structure was broken , it not  card=type-card");
     }
 }
 imagesParent.addEventListener("click", function(e){
@@ -30,6 +49,28 @@ imagesParent.addEventListener("click", function(e){
    if(type=="favorite"){
     e.target.classList.toggle("checked");
    }
+   if(type=="zoom"){
+       const img = e.target.closest(".photoselect__card").querySelector(".photoselect__img img");
+        zoomContainer.style="display:block";
+        const imgClone = img.cloneNode(true);       
+        zoomFigure.append(imgClone); 
+   }
 }) 
+
+//scroll top
+const goTop = document.querySelector(".photoselect__go-top");
+window.addEventListener("scroll", function(){
+  
+    if(window.pageYOffset>400){
+        goTop.classList.add("fixed-lg");
+    } else {
+        goTop.classList.remove("fixed-lg");
+    }
+   
+       
+})
+
+
+
 }())
 
