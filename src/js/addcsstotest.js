bc3ae4829131d90d391df8fb858e05c476d6c198
viktormoskalev07 <viktormoskalev07@gmail.com>
2021-09-24 18:46:57 +0300

    fix

---
+++ src/js/addcsstotest.js
@@ -0,0 +1,6 @@
+// https://viktormoskalev07.github.io/pikperfect-origin-newimages/build/css/upload.css
+
+
+const css = document.createElement('link');
+css.href="https://viktormoskalev07.github.io/pikperfect-origin-newimages/build/css/upload.css";
+document.body.append(css);
