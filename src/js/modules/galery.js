60d58d5f52f0272ce68ea194f7d7399d854dbaf8
MatviienkoNataliia <admin@MacBook-Air-Nataliia.local>
2022-02-15 19:07:56 +0200

    fix album 8.0

--- src/js/modules/galery.js
+++ src/js/modules/galery.js
@@ -5,24 +5,6 @@
   let close      ;
   let slidePlace ;
   let closeGalery;
-  if(!containerFlip && container){
-          activator = document.querySelectorAll('.gallery__activator-js');
-          activator.forEach(i => {
-            i.classList.add('cursor-pointer');
-          });
-          
-        close = document.querySelector('.gallery__close');
-        slidePlace =container.querySelector('.swiper-wrapper');
-        closeGalery=()=>{
-          container.classList.add('gallery__fade');
-          setTimeout(() => {
-              container.classList.add('gallery__d-none');
-          }, 400);   
-          GallerySlider.destroy(true, true);
-          slidePlace.innerHTML=''; 
-          document.body.classList.remove('overflow-h');
-        } 
-  }
   if(container && containerFlip){
     activator = document.querySelectorAll('.gallery__activator-js');
     activator.forEach(i => {
@@ -34,61 +16,19 @@
   closeGalery=()=>{
     container.classList.add('gallery__fade');
     containerFlip.classList.remove('shadow');
+    containerFlip.classList.remove('end');
     containerFlip.classList.remove('landscape');
     setTimeout(() => {
         container.classList.add('gallery__d-none');
     }, 400);   
     document.body.classList.remove('overflow-h');
-    document.removeEventListener('keydown', keyboardArrows); // keyboard input
   } 
 }
-
-  let slides;
-  let GallerySlider 
   
+/////////// flip book ////////////////
 const openGalery =function(){  
 
-    const folder = this.dataset.folder;
-    const images = this.dataset.galleryimages;
-    if(images){ 
-      const arrimages = images.split(',');  
-      slides='';
-      arrimages.forEach(imgName => { 
-        imgName = imgName.replace(/\s/g, ''); 
-        imgNameWebp = imgName.replace(/(\.png|\.jpg)/gm, ''); 
-          slides+=`
-          <div class="swiper-slide gallery__item">
-          <figure class="gallery__image"> 
-          <img   class="swiper-lazy"
-          data-src="${folder+imgName}"  
-          data-srcset="${folder}${imgNameWebp}-2880.webp 2x ,    ${folder}${imgNameWebp}-1440.webp" 
-          /> 
-          </figure> 
-          <div class="swiper-lazy-preloader swiper-lazy-preloader-white "></div>
-              </div>  
-          `; 
-        });
-        slidePlace.innerHTML=slides; 
-        setTimeout(() => {
-              GallerySlider   = new Swiper(".swiperGallery", { 
-                effect: "fade",
-                loop:true,
-                grabCursor:true,
-                resizeObserver:true,
-                lazy: {
-                    loadPrevNext: true,
-                  },
-                navigation: {
-                  nextEl: ".gallery__next",
-                  prevEl: ".gallery__prev",
-                }, 
-              });
-        }, 100);
-  
-    }
-
-/////////// flip book ////////////////
-    const app = document.querySelector('#app');
+  const app = document.querySelector('#app');
 
   if(app){
     let result = ' ';
@@ -151,7 +91,6 @@ const openGalery =function(){
       }
         
       // format album
-      
         if(i%2){
           result+= ` <div class="back">
                       <div class="outer">
@@ -381,4 +320,3 @@ const openGalery =function(){
 
 }());
 
-// @@include('flipBook.js');
