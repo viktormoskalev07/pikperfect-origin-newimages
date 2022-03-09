75478f596d785f19260bef35ede834a97fe4a2e3
MatviienkoNataliia <admin@MacBook-Air-Nataliia.local>
2022-03-09 15:58:43 +0200

    translated 12.0

---
+++ src/php_german/12.0/photographer_photo_albums_en.php
@@ -0,0 +1,266 @@
+<?php
+$this->headTitle('Hochzeitsalbum für Fotografen | PikPerfect');
+$this->headMeta()->appendName('description', 'Professionelle Hochzeitsalben & Fotobücher für den anspruchsvolle Fotografen. Inklusive Gestaltungsservice mit gratis Überarbeitungen. Neutraler Proofing-Link.');
+$region = $this->region()->getUserRegion();
+?>
+<section class="section-hero__image-right">
+    <div class="section-hero__wrap-flex">
+        <div class="section-hero__image-col px-0">
+            <figure class="figure-webp">
+                <picture data-priority="0" data-name="photographer-wedding-albums" data-path="12.0-photographer-photo-albums" class="picture-webp">
+                    <source class="toggle-img--js" srcset="/assets/images/towebp/12.0-photographer-photo-albums/min-photographer-wedding-albums.webp" type="image/webp">
+                    <img loading="lazy" class="nowebp-img" src="/assets/images/towebp/12.0-photographer-photo-albums/photographer-wedding-albums.jpg" alt="Hochzeitsalbum Profifotograf in Geschenkbox">
+                </picture>
+
+
+            </figure>
+        </div>
+        <div class="section-hero__content-col section-hero__content-col_gray-brown">
+            <div class="section-hero__bg">
+                <img src="/assets/images/towebp/image_texture.jpg" alt="bg-hero">
+            </div>
+            <h1 class="section-hero__span">Hochzeitsalben <br> & Fotobücher
+                <span>für professionelle Fotografen</span>
+            </h1>
+            <a href="#JoinUsToday" class="button" style="max-width: 265px; margin: 0 auto;">KONTO ERSTELLEN</a>
+        </div>
+    </div>
+</section>
+<section class="dignity dignity_blocks">
+    <div class="dignity__container">
+        <div class="dignity__wrap">
+            <div class="dignity__block">
+                <div class="dignity__img">
+                    <img src="/assets/images/circle_premium_materials.svg" alt="circle_premium_materials">
+                </div>
+                <div class="dignity__content">
+                    <h3>Professionelle Qualität</h3>
+                    <p>Hochwertige Fotoalben und Bücher für jedes Budget.</p>
+                </div>
+            </div>
+            <div class="dignity__block">
+                <div class="dignity__img">
+                    <img src="/assets/images/circle_album_design_service.svg" alt="circle_album_design_service">
+                </div>
+                <div class="dignity__content">
+                    <h3>Album Designservice</h3>
+                    <p>Kundenprüfung und Feedback auf neutraler Website</p>
+                </div>
+            </div>  
+            <div class="dignity__block">
+                <div class="dignity__img">
+                    <img src="/assets/images/circuit_trade_discounts.svg" alt="circuit_trade_discounts">
+                </div>
+                <div class="dignity__content">
+                    <h3>Händlerrabatte</h3>
+                    <p>Spezielle Handelspreise bei Fotoalben für Fotografen.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-reviews-card">
+    <div class="section-reviews-card__container">
+        <h2 class="section-reviews-card__title">Was andere Profi Fotografen sagen</h2>
+        <div class="swiper-container section-reviews-card__wrap">
+            <div class="swiper-wrapper">
+                <div class="section-reviews-card__card swiper-slide">
+                    <div class="section-reviews-card__content">
+                        <span class="section-reviews-card__stars">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                        </span>
+                        <p class="section-reviews-card__text">PikPerfect ist das GOTTESGESCHENK eines Unternehmens. Das Kundendienst- und Designteam sind unübertroffen. Als Hochzeitsfotograf lasse ich ständig Bücher von dieser Firma erstellen.</p>
+                    </div>
+                    <div class="section-reviews-card__user">
+                        <p class="name">Renee Schlittler</p>
+                        <p class="profession">Hochzeitsfotografin</p>
+                    </div>
+                </div>
+                <div class="section-reviews-card__card swiper-slide">
+                    <div class="section-reviews-card__content">
+                        <span class="section-reviews-card__stars">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                        </span>
+                        <p class="section-reviews-card__text">Ich bin Fotograf und finde oft nicht die Zeit, Fotobücher für meine Kunden zu erstellen. Es ist genial, dass ich nur die Fotos hochlade und PikPerfect das Album für mich gestaltet. Und ich die Möglichkeit habe, sie auch Änderungen vornehmen zu lassen.</p>
+                    </div>
+                    <div class="section-reviews-card__user">
+                        <p class="name">Riyard</p>
+                        <p class="profession">Professioneller Fotograf</p>
+                    </div>
+                </div>
+                <div class="section-reviews-card__card swiper-slide">
+                    <div class="section-reviews-card__content">
+                        <span class="section-reviews-card__stars">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                            <img src="/assets/images/icon-star.svg" alt="icon-star">
+                        </span>
+                        <p class="section-reviews-card__text">Ich arbeite als Profi-Fotografin mit PikPerfect für das Gestalten von Hochzeitsalben zusammen. Ich muss sagen, dass ich den Service aussergewöhnlich finde und mein Kunde den Freigabeprozess sehr einfach fand. Sowohl die Kunden als auch ich sind mit dem Endprodukt sehr zufrieden.</p>
+                    </div>
+                    <div class="section-reviews-card__user">
+                        <p class="name">Sara</p>
+                        <p class="profession">Professionelle Fotografin</p>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination swiperalbum-pagination"></div>
+        </div>
+        <a href="#JoinUsToday" class="button section-reviews-card__button">JETZT ANMELDEN</a>
+        <p class="section-reviews-card__bt-text">Um Details zu Sonderangeboten für Fotografen zu erhalten</p>
+    </div>
+</section>
+
+<section class="description-img">
+    <div class="description-img__container">
+        <div class="description-img__wrap">
+            <div class="description-img__img">
+                <figure style="padding-bottom: calc(713% / 788 * 100)" class="figure-webp">
+                    <picture data-priority="1" data-name="wedding-photographer-flush-mount-albums" data-path="12.0-photographer-photo-albums" class="picture-webp">
+                        <source class="toggle-img--js" srcset="/assets/images/towebp/12.0-photographer-photo-albums/min-wedding-photographer-flush-mount-albums.webp" type="image/webp">
+                        <img loading="lazy" class="nowebp-img" src="/assets/images/towebp/12.0-photographer-photo-albums/wedding-photographer-flush-mount-albums.jpg" alt="Hochwertiges Hochzeitsalbum für Profifotografen">
+                    </picture>
+
+                </figure>
+
+
+            </div>
+            <div class="description-img__content description-img__content_l-padding">
+                <h2 class="description-img__title">Premiumalbum</h2>
+                <p>Mit luxuriösen Einbänden und dicken Echtfotoseiten in Layflat Bindung gehört das Premium-Fotoalbum zum Besten vom Besten. Wähle zwischen Fujicolor Crystal Archive-Papieren oder Fine Art, um atemberaubende Doppelseiten für deine Kunden zu erstellen.</p>
+                <p class="description-img__price"><?php echo $this->translate('From') . ' ' . $this->format(
+                                                        $this->smallestPrices['premium'],
+                                                        $region->iso,
+                                                        2,
+                                                        $region->symbol
+                                                    ); ?>.</p>
+                <div class="description-img__buttons">
+                    <a href="<?php echo $this->languageUrl('upload-photos-', false, ['product' => 'premium'])?>" class="button">order</a>
+                    <a href="<?php echo $this->languageUrl('static-premium-layflat-wedding-albums-')?>" class="button button__gray">explore</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="description-img description-img__order">
+    <div class="description-img__container">
+        <div class="description-img__wrap">
+            <div class="description-img__img">
+                <figure style="padding-bottom: calc(713% / 788 * 100)" class="figure-webp">
+                    <picture data-priority="1" data-name="photographer-layflat-albums" data-path="12.0-photographer-photo-albums" class="picture-webp">
+                        <source class="toggle-img--js" srcset="/assets/images/towebp/12.0-photographer-photo-albums/min-photographer-layflat-albums.webp" type="image/webp">
+                        <img loading="lazy" class="nowebp-img" src="/assets/images/towebp/12.0-photographer-photo-albums/photographer-layflat-albums.jpg" alt="Echtfotoalbum für Fotografen">
+                    </picture>
+
+                </figure>
+
+
+            </div>
+            <div class="description-img__content description-img__content_m-padding">
+                <h2 class="description-img__title">Layflat Echtfotobuch</h2>
+                <p>Der Favorit in unserem Sortiment durch herausragende Qualität zum erschwinglichen Preis. Die Belichtung auf Silberhalogenidpapier sorgt für unglaubliche Farben und Klarheit. Perfekt für Hochzeits-, Schwangerschafts- und Baby-Fotoshootings.</p>
+                <p class="description-img__price"><?php echo $this->translate('From') . ' ' . $this->format(
+                                                        $this->smallestPrices['layflat'],
+                                                        $region->iso,
+                                                        2,
+                                                        $region->symbol
+                                                    ); ?>.</p>
+                <div class="description-img__buttons">
+                    <a href="<?php echo $this->languageUrl('upload-photos-', false, ['product' => 'layflat'])?>" class="button">order</a>
+                    <a href="<?php echo $this->languageUrl('static-layflat-wedding-photo-albums-')?>" class="button button__gray">explore</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="description-img">
+    <div class="description-img__container">
+        <div class="description-img__wrap">
+            <div class="description-img__img">
+                <figure style="padding-bottom: calc(713% / 788 * 100)" class="figure-webp">
+                    <picture data-priority="1" data-name="photo-books-for-photographers" data-path="12.0-photographer-photo-albums" class="picture-webp">
+                        <source class="toggle-img--js" srcset="/assets/images/towebp/12.0-photographer-photo-albums/min-photo-books-for-photographers.webp" type="image/webp">
+                        <img loading="lazy" class="nowebp-img" src="/assets/images/towebp/12.0-photographer-photo-albums/photo-books-for-photographers.jpg" alt="Offenes Fotobuch für Fotografen">
+                    </picture>
+
+                </figure>
+
+
+            </div>
+            <div class="description-img__content description-img__content_l-padding">
+                <h2 class="description-img__title">Digitales Fotobuch</h2>
+                <p>Die grossen Fotobücher können bis zu 1000 Bilder enthalten und sind unsere kostengünstigste Option. Das perfekte Fotobuch für den Kaffeetisch wird digital gedruckt auf mattem oder glänzendem Papier deiner Wahl.</p>
+                <p class="description-img__price"><?php echo $this->translate('From') . ' ' . $this->format(
+                                                        $this->smallestPrices['photobook'],
+                                                        $region->iso,
+                                                        2,
+                                                        $region->symbol
+                                                    ); ?>.</p>
+                <div class="description-img__buttons">
+                    <a href="<?php echo $this->languageUrl('upload-photos-', false, ['product' => 'standard'])?>" class="button">order</a>
+                    <a href="<?php echo $this->languageUrl('static-wedding-photo-books-')?>" class="button button__gray">explore</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-img m-auto">
+    <figure style="padding-bottom: calc(724% / 1440 * 100)" class="figure-webp">
+        <picture data-priority="2" data-name="professional-photobooks-for-photographers" data-path="12.0-photographer-photo-albums" class="picture-webp">
+            <source class="toggle-img--js" srcset="/assets/images/towebp/12.0-photographer-photo-albums/min-professional-photobooks-for-photographers.webp" type="image/webp">
+            <img loading="lazy" class="nowebp-img" src="/assets/images/towebp/12.0-photographer-photo-albums/professional-photobooks-for-photographers.jpg" alt="Paar schaut professionelles Fotobuch für Hochzeitsfotografen an">
+        </picture>
+
+    </figure>
+
+
+</section>
+<section class="section-text section-text_white section-text_m-padding">
+    <div class="section-text__container">
+        <h2 class="section-text__title">Spare Zeit & erhöh die Margen</h2>
+        <div class="section-text__content">
+            <h3 class="section-text__sub-title">Fotos hochladen, wir erledigen den Rest!</h3>
+            <p class="section-text__tx">Unser <span>Gestaltungsservice für professionelle Fotografen</span> erlaubt es dir, dich aufs Geschäft zu konzentrieren, wertvolle Zeit zu sparen und deinen Umsatz zu steigern.</p>
+            <h3 class="section-text__sub-title">Bequemes Kundenfeedback</h3>
+            <p class="section-text__tx">Du erhältst den Albumentwurf per Email: der Link führt zu einer Prüfungsseite ohne Logo oder Hinweis auf PikPerfect. So kannst du die Seite fürs Feedbackgeben direkt mit deinen Kunden teilen.</p>
+            <p class="section-text__tx">Alle Designs beinhalten unbegrenzte gratis Überarbeitungen. Wir geben das Fotoalbum erst in Druck, wenn du und deine Kunden vollkommen zufrieden sind.</p>
+        </div>
+    </div>
+</section>
+<?php if (!$this->isLogin()): ?>
+<section class="section-form section-form_gray" id="photographerRegistration">
+    <a name="JoinUsToday"></a>
+    <div class="section-form__container">
+        <h2 class="section-form__title">Jetzt beitreten</h2>
+        <p class="section-form__entered">Schliess dich den Fotografen an, die die mühevolle Gestaltung von Kundenalben auslagern und so mühelos zusätzliche Einnahmen erzielen. Profitiere von speziellen Handelspreisen und Musterrabatten.</p>
+
+        <input type="hidden" id="uploadPhotosLink" value="<?php echo $this->languageUrl('upload-photos-')?>">
+        <?php echo $this->render('partial/registration', ['isPhotographer' => true]) ?>
+    </div>
+</section>
+<?php endif ?>
+<section class="section-reviews reviews-widget">
+    <div class="container">
+        <h2 class="d-none"> Bewertungen</h2>
+        <?php echo $this->partial('application/partial/reviews_widget', ['product' => 'wedding-album']) ?>
+    </div>
+</section>
+<section class="section-text section-text_entered section-text_light">
+    <div class="section-text__container">
+        <h2 class="section-text__title">Mehr über die Hochzeitsalben für Fotografen von PikPerfect</h2>
+        <div class="section-text__content">
+            <p>Bei PikPerfect kannst du professionelle Hochzeitsalben und <a href="<?php echo $this->languageUrl('static-photo-books-') ?>" class="link" target="_blank">Fotobücher</a> erstellen lassen, ohne stundenlang auf den Bildschirm zu starren. Bildbearbeitung ist schon genug Arbeit! Das Beste daran ist, dass du die volle kreative Kontrolle über Stil und Designvorlieben behältst, während deine Kunden über einen neutralen Proofing-Link ihr Feedback abgeben.</p>
+            <p>Hochwertige <a href="<?php echo $this->languageUrl('static-leather-photo-albums-') ?>" class="link" target="_blank">Hochzeitsalben aus Leder</a> für Fotografen sind ziemlich teuer in der Herstellung, ganz zu schweigen davon, sie als profitables Produkt an einen Kunden zu verkaufen. Viele Paare entscheiden sich am Ende für ein günstigeres Fotobuch oder verzichten sogar ganz auf ein Album. Bei PikPerfect bieten wir den perfekten Kompromiss mit Alben, die aus den besten Materialien handgefertigt werden, um Erbstücke herzustellen, die deine Kunden ein Leben lang schätzen werden.</p>
+        </div>
+    </div>
+</section>
+
